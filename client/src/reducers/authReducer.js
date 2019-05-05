0dcec70993e5960ccdb037bffef801bbcdb89f66
ivancab <ivo.cabrera29@gmail.com>
2019-05-05 12:41:22 -0300

    emaily update

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,15 @@
+import { FETCH_USER } from "../actions/types";
+
+let initialState = {
+  auth: [],
+};
+
+export default function(state = null, action){
+  console.log(action);
+  switch(action.type) {
+    case FETCH_USER:
+      return action.payload || false;
+    default:
+      return state;
+  }
+}
