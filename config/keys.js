f544bc516d07734fcaeffca97034ecc9ffc796fb
ivancab <ivo.cabrera29@gmail.com>
2019-05-05 13:21:10 -0300

    keys

---
+++ config/keys.js
@@ -0,0 +1,34 @@
+// Google oauth
+// Client Id
+// 700815825249-9ga8k4sjo3nrn39lnvc7lolavmm65jkv.apps.googleusercontent.com
+// secret
+// H5AkJhlm-4luDl4ksDbsafUc
+
+module.exports = {
+  googleClientID: '700815825249-9ga8k4sjo3nrn39lnvc7lolavmm65jkv.apps.googleusercontent.com',
+  googleCLientSecret: 'H5AkJhlm-4luDl4ksDbsafUc',
+  mongoURI: 'mongodb://adminCabrera:Icabp430667092@ds115762.mlab.com:15762/emaily-dev-cabrera',
+  cookieKey: 'lkfnalksfmalñssssfewwfewefwefnjnbasdvgahb',
+};
+
+/**
+ * Heroku
+ *  protected-temple-81987
+    https://protected-temple-81987.herokuapp.com/ | https://git.heroku.com/protected-temple-81987.git 
+ * 
+ * 
+ */
+
+
+/**
+ *  PROD KEYS
+ *  DB USER: emaily-prod
+ *  PASSWORD: pLAw9Z2c4tWz8Gp
+ *
+ *  MLAB URL: mongodb://emaily-prod:pLAw9Z2c4tWz8Gp@ds231951.mlab.com:31951/emaily-prod
+ * 
+ * 
+ * google oauth client id: 814616894568-ttp4d0eul76qo5km99oqnbo8mrtd66jo.apps.googleusercontent.com
+ * secret: 814616894568-ttp4d0eul76qo5km99oqnbo8mrtd66jo.apps.googleusercontent.com
+ * 
+ * */
